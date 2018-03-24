f9dc86c3ff8a496c0355d0f4368a86e91193e38b
javier <jbenavi2@asu.edu>
2018-03-24 01:17:59 -0700

    complete

--- thread_test.c
+++ thread_test.c
@@ -1,3 +1,12 @@
+/*
+ * Javier Benavides
+ * CSE 330 Project 1
+ * Due 2/16/2018
+ * */
+
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -5,34 +14,54 @@
 #include "q.h"
 
 TCB_t *RunQ;  //global header RunQ
+int a = 0;
 
 void thread1(){
     int x = 0;
 
     while (1){
-        printf("x = %d\n", x);
+        printf("Thread 1\n");
+        printf("local x = %d\n", x);
+        printf("global a = %d\n\n", a);
         x++;
+        a++;
         yield();
-        sleep(1);
+        sleep(2);
     }
 }
 
 void thread2(){
     int y = 5;
     while(1){
-        printf("y = %d\n", y);
+        printf("Thread 2\n");
+        printf("local y = %d\n", y);
+        printf("global a = %d\n\n", a);
         y++;
+        a++;
         yield();
-        sleep(1);
+        sleep(2);
+    }
+}
+
+void thread3(){
+    int z = 10;
+    while(1){
+        printf("Thread 3\n");
+        printf("local z = %d\n", z);
+        printf("global a = %d\n\n", a);
+        z++;
+        a++;
+        yield();
+        sleep(2);
     }
 }
 
 int main() {
     TCB_t *RunQ;
-    //RunQ = malloc(sizeof(TCB_t));
     InitQueue(&RunQ);
     start_thread(thread1);
     start_thread(thread2);
+    start_thread(thread3);
     run();
 
 /// These are the previous tests done for part 1
@@ -82,6 +111,6 @@ int main() {
 
     
 
-    printf("hello, world");
+    //printf("hello, world");
     return 0;
 }
